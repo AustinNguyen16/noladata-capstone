7b93484f6dd4e7904cd700803b51ee752a5049ba
jtmcdermott9 <90660038+jtmcdermott9@users.noreply.github.com>
2024-02-21 14:35:16 -0600

    map to table callback works!

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ app.layout = html.Div(children=[
     #Table Component
     html.Div(
         dash_table.DataTable(
-            id='example-table',
+            id='311-table',
             columns=[{'name': col, 'id': col} for col in data_display.columns],  # Define columns for the table
             data=data_display.to_dict('records'),  # Convert DataFrame to a format suitable for DataTable
             #style_table={'width': '1%', 'margin': '300px', 'marginTop': '20px'}  # Style for the table
@@ -66,6 +66,7 @@ app.layout = html.Div(children=[
             
             filter_action = 'native', #This sets data filter to native dash type, which is a typed filter system, not menu type which may be desirable
                                       #Also need to specify type to enable gt, lt, eq comparison for numeric types
+            filter_query= ''
     ),
     style={'position': 'relative', 'width': '10%', 'height': '50vh'}
     ),
@@ -76,10 +77,44 @@ app.layout = html.Div(children=[
 
 #Callbacks - these make the Dash app interactive by updating Outputs when Inputs change
 
+#Map -> Table - this actually works!!
+@callback(
+        Output('311-table', 'data'),
+        Input('new-orleans-map', 'selectedData')
+)
+def update_table(selectedData):
+    if selectedData is None:
+        return data.to_dict('records')
+    
+    # Filter data based on selected points on the map
+    selected_points = [point['pointIndex'] for point in selectedData['points']]
+    filtered_df = data.iloc[selected_points]
+    
+    return filtered_df.to_dict('records')
 
-#Attempt #3 of this callback: 
+#Table -> Map 
+@callback(
+    Output('new-orleans-map', 'figure'),
+    Input('311-table', 'filter_query'),
+    #State('311-table', 'data')
+)
 
+def update_map(filter_query):
+    filtered_data = data.query(filter_query)
 
+    figure = px.scatter_mapbox(
+        filtered_data,
+        lat='latitude',
+        lon='longitude',
+        mapbox_style='carto-positron',
+        center=new_orleans_coordinates,
+        title='311 Code Enforcement Data 2012-Present',
+        zoom=10
+    )
+
+    return figure
+
+    
 
 
 # Run the app
